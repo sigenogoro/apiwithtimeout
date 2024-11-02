893ed2ea1c642a2981a7d64a2bd03dbf153b2479
sigenogoro <25719873+sigenogoro@users.noreply.github.com>
2024-11-03 00:42:02 +0900

    first commit

---
+++ src/main/kotlin/com/example/apiwithtimeout/ApiwithtimeoutApplication.kt
@@ -0,0 +1,11 @@
+package com.example.apiwithtimeout
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ApiwithtimeoutApplication
+
+fun main(args: Array<String>) {
+	runApplication<ApiwithtimeoutApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/apiwithtimeout/ApiwithtimeoutApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.apiwithtimeout
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ApiwithtimeoutApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
